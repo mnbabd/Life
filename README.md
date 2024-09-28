511e9e5bbc8c833bb7500dfa69288f13cb4a3308
Muneeb Abid <mnb.abd@gmail.com>
2024-09-28 15:09:05 +1000

    Initial project setup

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Life
 Let life play out as you walk away from your screens
 
+
+```
+sudo pacman -Syu base-devel glibc libc++ libc++abi linux-api-headers
+```
+
+```
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
---
+++ main.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file main.cpp
+ * @author Muneeb (mnb.abd@gmail.com)
+ * @brief Look for the main purpose of life. Some say it is a stream of stimuli triggered by a chain of events.
+ * 
+ * NOTE: This repository and code-base may not contain the most helpful comments if you are trying to make sense of things.
+ * 
+ * Then again, search for meaning is never easy.
+ * 
+ * @copyright Copyright Muneeb (c) 2024. All Rights Reserved.
+ * 
+ */
+
+#include <iostream>
+
+/**
+ * @brief What may cause an end to main. What is the meaning of what's left behind.
+ */
+enum class life_returns : int
+{
+    screech = -1, /*!< Life has collapsed in on itself. An unforeseen annihilation. */
+    move_on = 0,  /*!< Peacefully moved on. What some may consider a Success. */
+};
+
+
+/**
+ * @brief You may give various arguments. It rarely makes a difference, but that doesn't mean it never does.
+ * 
+ * @param argc - How much will you argue?
+ * @param argv - If only I had a place to put all these arguments.
+ * @return int -  What is the meaning of what's left behind? Will life return? It has to!
+ */
+int main(int argc, char* argv[])
+{
+
+}
